6003e8d3048bc87bf099b3e0765d37b54c6650d0
envelope01 <vivekkher07@gmail.com>
2026-01-01 18:15:18 +0530

    updated url after deploying backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 /* =========================================================
    API CONFIGURATION
    ========================================================= */
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://saloncrm-cok4.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
